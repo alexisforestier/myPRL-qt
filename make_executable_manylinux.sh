890f06dff648015549b89ef03377a26fb46a8499
Alexis Forestier <alforestier@gmail.com>
2023-05-27 01:10:57 +0200

    new version with classes HPData and HPCalibrations. Minimal usage, no table. Executable works on linux

--- make_executable_manylinux.sh
+++ make_executable_manylinux.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-rm -rf .venv/ __pycache__/ build/ dist/ myPRL_qt.spec
+rm -rf .venv/ __pycache__/ build/ dist/ myPRL-qt.spec
 python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
-python3 -m PyInstaller myPRL_qt.py
+python3 -m PyInstaller myPRL-qt.py
 deactivate
